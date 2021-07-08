d34de2bd5012d2732dae2ef59f6817b0e56c66ca
Lcviegas1987 <luisclaudio87@gmail.com>
2021-07-08 15:24:13 -0300

    exercicio aula 1

---
+++ 01-exercicio.rb
@@ -0,0 +1,14 @@
+puts "quantas horas há em um ano?"
+puts  (365 * 24).to_s + " horas em um ano"
+puts 
+puts "quantos minutos há em uma decada?"
+puts  (3650 * 1440).to_s + " minutos em uma decada"
+puts
+puts "qual a sua idade em segundos?"
+puts  (31536000 * 33) .to_s + " segundos"
+puts
+puts "quantos chocolates voce pretende comer na vida"
+puts  (6 * 50) .to_s + " chocolates que pretendo comer na vida"
+puts
+puts "Desafio: se a minha idade é 1232 milhoes de segundos, qual a minha idade em anos?"
+puts  (1232000000 / 31536000) .to_s + " anos "
