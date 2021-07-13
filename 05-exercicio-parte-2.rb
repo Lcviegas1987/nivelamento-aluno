b9fa5cbd18d35c6ace8816188e03d6be592bd829
Lcviegas1987 <luisclaudio87@gmail.com>
2021-07-13 15:19:58 -0300

    parte 2 exercicio 5 corrigida

--- 05-exercicio-parte-2.rb
+++ 05-exercicio-parte-2.rb
@@ -1,14 +1,14 @@
 print "Por informe seu nome: "
-nome = gets 
+nome = gets.chomp
 
 
 print "Agora informe sua idade: "
 idade = gets.to_f
 
 def calcula_estimativa_batimentos(idade)
-    return idade * 365 * 24 * 60 *60
+    return idade * 365 * 24 * 60 * 80
 end
 puts calcula_estimativa_batimentos(idade)
-puts nome + "sua idade é " + idade.to_s + " seu coracao bateu até agora :" + calcula_estimativa_batimentos(idade).to_s
+puts nome + " sua idade é " + idade.to_s + " seu coracao bateu até agora :" + calcula_estimativa_batimentos(idade).to_s
 
 
