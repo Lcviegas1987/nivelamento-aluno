33faeaeefdbfb378e60453a33b9eae7d261691c6
Lcviegas1987 <luisclaudio87@gmail.com>
2021-07-13 13:23:39 -0300

    exercico 05 parte 1

---
+++ 05-exercicio-parte-1.rb
@@ -0,0 +1,29 @@
+def calcula_diametro(raio)
+    return raio * 2
+end
+print "informe o valor do raio "
+raio = gets
+raio = raio.to_f
+puts calcula_diametro(raio)
+
+def calcula_comprimento(raio)
+    return calcula_diametro(raio) * Math::PI 
+end
+puts calcula_comprimento(raio)
+
+def calcula_area(raio)
+    return raio ** 2 * Math::PI
+end
+puts calcula_area(raio)
+
+
+
+
+
+
+
+
+
+
+
+
