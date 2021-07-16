b19f0cbd07a9eefbdb71c29aafe10436c3b1cf36
Lcviegas1987 <luisclaudio87@gmail.com>
2021-07-16 11:24:36 -0300

    08-exercicio-parte1.rb ajustado

--- 08-exercicio-parte1.rb
+++ 08-exercicio-parte1.rb
@@ -1,11 +1,9 @@
-puts "Programa para calcular a tabuada "
-print "Digite um numero para calcular a tabuada "
-numero = gets.chomp.to_i
 def calcula_tabuada(numero)
-    n = 1
-    while n <=10
-        puts numero.to_s + "x" + n.to_s + "=" + (numero * n).to_s
-        n = n+1
+    for n in (1..10)
+        puts numero.to_s + " x " + n.to_s + " = " + (numero * n).to_s
     end
 end
+puts "Programa para calcular a tabuada "
+print " Digite o numero que deseja saber a tabuada "
+numero = gets.chomp .to_i
 calcula_tabuada(numero)
