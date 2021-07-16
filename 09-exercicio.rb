f41a6cd676b07b7b50d6ccddf26c91247f3c75a6
Lcviegas1987 <luisclaudio87@gmail.com>
2021-07-16 16:46:42 -0300

    09-exercicio.rb

---
+++ 09-exercicio.rb
@@ -0,0 +1,28 @@
+puts "Bem-vindo ao jogo de adivinhação!"
+puts "O computador pensou em um número entre 0 e 100. Tente adivinhar!"
+
+numero = rand(0..100)
+puts "(" + numero.to_s + ")"
+tentativas = 0
+acertou = false
+while not acertou
+    print "Digite um numero: "
+    chute = gets.chomp
+
+if chute == "0" || (chute.to_i >= 1 && chute.to_i <= 100)
+    tentativas = tentativas + 1
+    if numero == chute.to_i
+        puts "Acertou!"
+        acertou = true
+    else
+    if chute .to_i > numero
+        puts "O número chutado foi maior que o pensado, tente novamente : "
+    else
+        puts "O número chutado foi menor que o pensado, tente novamente: "
+    end
+end
+else 
+    puts "Chute invalido, tente novamente "
+end
+end
+puts "Você usou " + tentativas.to_s + " tentativas. Game Over "
--- teste.tabuada.rb
+++
@@ -1,9 +0,0 @@
-def calcula_tabuada(numero)
-    for n in (1..10)
-        puts numero.to_s + " x " + n.to_s + " = " + (numero * n).to_s
-    end
-end
-puts "Programa para calcular a tabuada "
-print " Digite o numero que deseja saber a tabuada "
-numero = gets.chomp .to_i
-calcula_tabuada(numero)
