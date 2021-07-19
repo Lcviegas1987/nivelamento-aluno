a89bc63c660574e5afc8f591c35c936f60476192
Lcviegas1987 <luisclaudio87@gmail.com>
2021-07-18 21:36:39 -0300

    10-exercicio-parte2.rb

---
+++ 10-exercicio-parte2.rb
@@ -0,0 +1,82 @@
+#2) Esse aqui é um desafio. Faça um código para obter esse resultado:
+        # *
+        # **
+        # ***
+        # ****
+        # *****
+        # ******
+        # *******
+        # ********
+        # *********
+        # **********
+
+ 
+        for x in (1..1)
+                for y in (1..1)
+                    print "*"
+                end
+                puts 
+            end
+            
+            for x in (1..1)
+                for y in (2..3)
+                    print "*"
+                end
+                puts 
+            end
+            
+            for x in (1..1)
+                for y in (2..4)
+                    print "*"
+                end
+                puts 
+            end
+            
+            for x in (1..1)
+                for y in (2..5)
+                    print "*"
+                end
+                puts 
+            end
+            
+            for x in (1..1)
+                for y in (2..6)
+                    print "*"
+                end
+                puts 
+            end
+            
+            for x in (1..1)
+                for y in (2..7)
+                    print "*"
+                end
+                puts 
+            end
+            
+            for x in (1..1)
+                for y in (2..8)
+                    print "*"
+                end
+                puts
+            end
+            
+            for x in (1..1)
+                for y in (2..9)
+                    print "*"
+                end
+                puts
+            end
+            
+            for x in (1..1)
+                for y in (2..10)
+                    print "*"
+                end
+                puts 
+            end
+            
+            for x in (1..1)
+                for y in (2..11)
+                    print "*"
+                end
+                puts 
+            end
