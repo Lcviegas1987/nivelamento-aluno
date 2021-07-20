1f97b77b1e530148d21fb8e4f2663c2fd99713db
Lcviegas1987 <luisclaudio87@gmail.com>
2021-07-20 14:19:58 -0300

    exercicio 13 corrigido

--- 13-exercicio.rb
+++ 13-exercicio.rb
@@ -1,15 +1,25 @@
+def exibe_array(array)
+    for n in (0..array.size-1)
+        print array[n]
+        if n < array.size-1
+            print ","
+        end
+    end
+    puts
+end
+
 def remove_da_posicao(array, posicao_alvo)
-    if posicao_alvo >(array.size-2 || posicao<=0)
+    if posicao_alvo > (array.size-2 || posicao<=0)
         return array
     else
         novo_array = []
-        for n in (0..array.size-1)
+        for n in (1..array.size-1)
         novo_array[n-1] = array[n]
         end
         return novo_array
     end
 end
 
-numeros = [2, 4, 6, 8, 10, 12]
+numeros = [5, 10, 15, 20, 25]
 puts
-remove_da_posicao(numeros,2)
+print remove_da_posicao(numeros,0)
